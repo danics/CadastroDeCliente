a19c708e774edc48fcff1d31c62e1bbf4ca7201d
Dani Santos <dani_cs01@hotmail.com>
2019-07-25 21:20:05 -0300

    Add Controller e Views Cliente

--- CadastroDeClientes/Data/ApplicationDbContext.cs
+++ CadastroDeClientes/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CadastroDeClientes.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CadastroDeClientes.Data
