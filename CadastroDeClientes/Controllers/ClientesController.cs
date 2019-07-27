c3c606b1e467f8e2003d4e16894733d8f00048f5
Dani Santos <dani_cs01@hotmail.com>
2019-07-27 12:06:19 -0300

    Add Funcoes de Edicao, Exclusao e respectivas Views

--- CadastroDeClientes/Controllers/ClientesController.cs
+++ CadastroDeClientes/Controllers/ClientesController.cs
@@ -4,26 +4,108 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CadastroDeClientes.Models;
+using CadastroDeClientes.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CadastroDeClientes.Controllers
 {
+    [Authorize]
     public class ClientesController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _contexto;
+
+        public ClientesController(ApplicationDbContext contexto)
         {
-            return View();
+            _contexto = contexto;
         }
 
+        //INDEX
+        public async Task<IActionResult> Index()
+        {
+            return View(await _contexto.Clientes.ToListAsync());
+        }
+
+        //GET CREATE
         [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
 
+        //POST CREATE
         [HttpPost]
-        public IActionResult Create(Cliente cliente)
+        public async Task<IActionResult> Create(Cliente cliente)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                _contexto.Add(cliente);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(cliente);
+        }
+
+        //GET EDIT
+        public async Task<IActionResult> Edit (int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _contexto.Clientes.FindAsync(Id);
+
+            if(cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        //POST EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit (int Id, Cliente cliente)
+        {
+          
+            if (ModelState.IsValid)
+            {
+                _contexto.Update(cliente);
+                await _contexto.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        //GET DELETE
+        public async Task<IActionResult> Delete(int? Id)
+        {
+            if(Id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _contexto.Clientes.FindAsync(Id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var cliente = await _contexto.Clientes.FindAsync(Id);
+            _contexto.Clientes.Remove(cliente);
+            await _contexto.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- CadastroDeClientes/Models/Cliente.cs
+++ CadastroDeClientes/Models/Cliente.cs
@@ -1,20 +1,28 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CadastroDeClientes.Models
 {
     public class Cliente
     {
         public int Id { get; set; }
+        [DisplayName("Nome")]
         public string PrimeiroNome { get; set; }
         public string Sobrenome { get; set; }
         public string Cpf { get; set; }
+        [DisplayName("DDD")]
         public string TelefoneDDD { get; set; }
+        [DisplayName("Telefone")]
         public string TelefoneNumero { get; set; }
         public string Endereco { get; set; }
         public string Bairro { get; set; }
         public string Cidade { get; set; }
+        [DisplayName("Numero")]
         public string EnderecoNumero { get; set; }
         public string Cep { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayName("Data de Nascimento")]
         public DateTime? DataDeNascimento { get; set; }
         public string Email { get; set; }
 
